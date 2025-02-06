0293f395c981f81d99f9691fea2607623829ae0c
david_smith <david_smith@sweetwater.com>
2025-02-06 11:38:29 -0500

    main Updated `src/SpapiOrders.php`.

--- src/SpapiOrders.php
+++ src/SpapiOrders.php
@@ -400,14 +400,21 @@ class SpapiOrders
         curl_setopt_array(
             $CurlHandle,
             [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
                 CURLOPT_HTTPHEADER => array_merge(
                     [
+                        'accept: application/json',
                         'x-amz-date: '.gmdate('Ymd\THis\Z'),
                         'user-agent: '.($user_agent ?: '(Language=PHP/'.PHP_VERSION.'; Platform='.php_uname('s').'/'.php_uname('r').')')
                     ],
                     $headers
                 ),
-                CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_HEADER => true,
             ] + $options
         );
