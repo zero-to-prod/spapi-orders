b33ab8f9b505f3351836f9393f913d272ef166ee
David Smith <dasmith@zero-to-prod.com>
2025-02-20 16:59:58 -0500

    feat/v3 Updated `tests/Unit/ExampleTest.php`.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,12 +3,33 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Zerotoprod\SpapiOrders\SpapiOrders;
+use Zerotoprod\SpapiOrders\Support\Testing\SpapiOrdersFake;
+use Zerotoprod\SpapiOrders\Support\Testing\SpapiOrdersResponseFactory;
 
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true(): void
+    public function fakes_response(): void
     {
-        $this->assertTrue(true);
+        $response = SpapiOrdersFake::fake(['response' => ['payload' => ['order' => 1]]]);
+
+        SpapiOrders::from('access_token')->getOrder('123-1234567-1234567');
+
+        $this->assertEquals(1, $response->getOrder('123-1234567-1234567')['response']['payload']['order']);
+    }
+
+    /** @test */
+    public function using_factory(): void
+    {
+        $response = SpapiOrdersFake::fake(
+            SpapiOrdersResponseFactory::factory([
+                'response' => ['payload' => ['order' => 1]]
+            ])->make()
+        );
+
+        SpapiOrders::from('access_token')->getOrder('123-1234567-1234567');
+
+        $this->assertEquals(1, $response->getOrder('123-1234567-1234567')['response']['payload']['order']);
     }
 }
