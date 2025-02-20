65fcf27f150494c6df3eb9325d1d20ab15492cdc
David Smith <dasmith@zero-to-prod.com>
2025-02-20 17:00:44 -0500

    feat/v3 Updated `src/Support/Testing/SpapiOrdersFake.php`.

--- src/Support/Testing/SpapiOrdersFake.php
+++ src/Support/Testing/SpapiOrdersFake.php
@@ -10,6 +10,9 @@ use Zerotoprod\SpapiOrders\Contracts\SpapiOrdersInterface;
  */
 class SpapiOrdersFake implements SpapiOrdersInterface
 {
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function __construct(array $response = [])
     {
         $this->response = $response;
@@ -31,31 +34,49 @@ class SpapiOrdersFake implements SpapiOrdersInterface
         return $instance;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrders(array $MarketplaceIds, ?string $CreatedAfter = null, ?string $CreatedBefore = null, ?string $LastUpdatedAfter = null, ?string $LastUpdatedBefore = null, ?array $OrderStatuses = null, ?array $FulfillmentChannels = null, ?array $PaymentMethods = null, ?string $BuyerEmail = null, ?string $SellerOrderId = null, ?int $MaxResultsPerPage = null, ?array $EasyShipShipmentStatuses = null, ?array $ElectronicInvoiceStatuses = null, ?string $NextToken = null, ?array $AmazonOrderIds = null, ?string $ActualFulfillmentSupplySourceId = null, ?bool $IsIspu = null, ?string $StoreChainStoreId = null, ?string $EarliestDeliveryDateBefore = null, ?string $EarliestDeliveryDateAfter = null, ?string $LatestDeliveryDateBefore = null, ?string $LatestDeliveryDateAfter = null, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrder(string $orderId, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrderBuyerInfo(string $orderId, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrderAddress(string $orderId, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrderItems(string $orderId, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @link https://github.com/zero-to-prod/spapi-orders
+     */
     public function getOrderItemsBuyerInfo(string $orderId, array $options = []): array
     {
         return $this->response;
